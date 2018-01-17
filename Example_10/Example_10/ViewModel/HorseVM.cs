5a20b6f9c3d4c3ab6f865c54cb0cce6de63907ae
Ian <ianburrows.sa@gmail.com>
2018-01-17 13:05:42 +0100

    Finished Need to add server/client

---
+++ Example_10/Example_10/ViewModel/HorseVM.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_10.ViewModel
+{
+    public class HorseVM:ViewModelBase
+    {
+
+        public int Speed { get; set; }
+        public string Name { get; set; }
+        public HorseVM(int speed, string name)
+        {
+            Speed = speed;
+            Name = name;
+        }
+    }
+}
--- Example_10/Example_10/ViewModel/MainViewModel.cs
+++ Example_10/Example_10/ViewModel/MainViewModel.cs
@@ -1,34 +1,49 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.ObjectModel;
 
 namespace Example_10.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
+
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        private string _fastestHorse;
+
+        public RelayCommand AddBtn { get; set; }
+        public int Speed { get; set; }
+        public string Name { get; set; }
+        public ObservableCollection<HorseVM> HorseCollection { get; set; }
+        public string FastestHorse {
+            get { return _fastestHorse; }
+            set { _fastestHorse = value; RaisePropertyChanged(); }
+        }
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            HorseCollection = new ObservableCollection<HorseVM>();
+            AddBtn = new RelayCommand(() =>
+            {
+                HorseCollection.Add(new HorseVM(Speed, Name));
+                GiveFastestHorse();
+            });
+        }
+
+        private void GiveFastestHorse()
+        {
+            int maxHorse = 0;
+            foreach (var item in HorseCollection)
+            {
+                maxHorse = Math.Max(item.Speed, maxHorse);
+            }
+
+            foreach (var item in HorseCollection)
+            {
+                if (item.Speed == maxHorse)
+                {
+                    FastestHorse = item.Name;
+                }
+            }
         }
     }
 }
